e67ad0714b768309619c8867f706767333104ac9
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-24 22:52:37 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,25 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux/es/exports';
-import { setFilter } from '../../redux/contacts/slice';
-import { getFilter } from '../../redux/contacts/selectors';
+import { setStatusFilter } from '../../redux/filtersSlice';
+import { getStatusFilter } from '../../redux/selectors';
 import { FilterWrap, FilterLabel, FilterInput } from './Filter.styled';
 
 const Filter = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(getFilter);
+  const filters = useSelector(getStatusFilter);
+
+  const onChange = e => {
+    dispatch(setStatusFilter(e.target.value));
+  };
 
   return (
     <FilterWrap>
       <FilterLabel>
         Find contacts by name
-        <FilterInput
-          value={filter}
-          type="text"
-          name="filter"
-          onChange={e => dispatch(setFilter(e.target.value))}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+        <FilterInput value={filters} type="text" onChange={onChange} />
       </FilterLabel>
     </FilterWrap>
   );
