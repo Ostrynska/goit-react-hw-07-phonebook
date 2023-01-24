0136c50e710ddabd4478ad2fa3b8d2feef48bc02
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-24 22:52:34 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,46 +1,42 @@
 import PropTypes from 'prop-types';
-// import { useSelector } from 'react-redux';
-// import { useDispatch } from 'react-redux';
-// import { removeContacts } from '../../redux/contacts/slice';
-// import { getContacts, getFilter } from '../../redux/contacts/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/operations';
+import { getFilteredContacts } from 'redux/selectors';
+
 import {
   ContactsList,
-  //   ContactsListItem,
-  //   ContactsListInf,
-  //   ContactsListButton,
+  ContactsListItem,
+  ContactsListInf,
+  ContactsListButton,
 } from './ContactList.styled';
-// import { IoTrashBinOutline } from 'react-icons/io5';
-// import { IconContext } from 'react-icons';
-// import { MdContactPage } from 'react-icons/md';
+import { IoTrashBinOutline } from 'react-icons/io5';
+import { IconContext } from 'react-icons';
+import { MdContactPage } from 'react-icons/md';
 
 const ContactList = () => {
-  //   const dispatch = useDispatch();
-  //   const contacts = useSelector(getContacts);
-  //   const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+  const contacts = useSelector(getFilteredContacts);
 
-  //   const filterContacts = contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(filter.toLowerCase())
-  //   );
   return (
     <ContactsList>
-      {/* {filterContacts.map(({ id, name, number }) => (
+      {contacts.map(({ id, name, phone }) => (
         <ContactsListItem key={id}>
           <IconContext.Provider value={{ color: '#d85841' }}>
             <MdContactPage />
           </IconContext.Provider>
           <ContactsListInf>
             <b>{name}: </b>
-            {number}
+            {phone}
           </ContactsListInf>
           <ContactsListButton
             type="submit"
             name={name}
-            onClick={() => dispatch(removeContacts(id))}
+            onClick={() => dispatch(deleteContact(id))}
           >
             <IoTrashBinOutline />
           </ContactsListButton>
         </ContactsListItem>
-      ))} */}
+      ))}
     </ContactsList>
   );
 };
@@ -50,7 +46,7 @@ ContactList.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired,
     })
   ),
 };
